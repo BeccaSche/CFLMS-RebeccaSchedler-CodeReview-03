c1fa500373afbe015c9896d3e4ddfd3810d08120
Rebecca Schedler <rebecca.schedler@gmx.net>
2020-05-30 16:47:42 +0200

    Index final version, Team subpage still in work

---
+++ script/team.js
@@ -0,0 +1,30 @@
+class employee {
+  constructor(name, role, email, hobby) {
+    this.Name = name
+    this.role = role
+    this.email = email
+    this.hobby = hobby
+  }
+  }
+
+
+let employee1 = new employee("Kermit the Frog", "CEO", "kermit@cfinsurance.at", "Singing")
+let employee2= new employee("Miss Piggy", "CFO", "piggy@cfinsurance.at", "Reading")
+let employee3 = new employee("Fozzy the Bear", "Consultant", "fozzy@cfinsurance.at", "Golfe")
+
+// console.log(employee3)
+/*entries.forEach(entry => {
+    let key = entry[0];
+    let value = entry[1];
+
+    console.log(`${key}: ${value}`);
+});*/
+
+var person = document.querySelectorAll("figure")
+Person.addEventListener('mouseover', infoPerson, false);
+
+    // function infoPerson() {
+		// var text= ' '
+
+
+// }
